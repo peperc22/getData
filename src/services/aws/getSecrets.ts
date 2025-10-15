60bb78c82e783390e0748133fc7f803b29cc1858
Pepe <j.reyes@solucionescomsys.com>
2025-10-15 17:36:27 -0600

    [FEAT] retrieve secrets from aws

---
+++ src/services/aws/getSecrets.ts
@@ -0,0 +1,16 @@
+import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
+
+const client = new SecretsManagerClient();
+
+export const getSecret = async (secretName: string) => {
+  try {
+    const response = await client.send(new GetSecretValueCommand({
+      SecretId: secretName
+    }));
+
+    const secretString = response.SecretString ?? null;
+    return secretString ? JSON.parse(secretString) : (() => { throw new Error('Secret not found'); })();
+  } catch (error) {
+    throw new Error(`Failed to fetch secret ${secretName}: ${error}`);
+  }
+}
