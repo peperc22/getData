87fbe85b7a7924393e72cf94cc8acf4af317f8fa
Pepe <j.reyes@solucionescomsys.com>
2025-10-15 17:37:07 -0600

    [FEAT] get unit if from vin

---
+++ src/services/wialon/getUnitId.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { WIALON_BASE_URL } from "../../config/config";
+
+export const getUnitId = async (vin: string, sid: string) => {
+    const params = {
+        spec: {
+            itemsType: "avl_unit",
+            propName: "rel_customfield_value",
+            propValueMask: vin,
+            sortType: ""
+        },
+        force: 1,
+        flags: 1,
+        from: 0,
+        too: 0,
+    };
+    const url = `${WIALON_BASE_URL}?svc=core/search_items&params=${JSON.stringify(params)}&sid=${sid}`;
+
+    try {
+        const response = await axios.get(url);
+        const unitId = response.data.items[0].id;
+
+        return unitId;
+    } catch (error) {
+        throw new Error(`Wialon error: ${error}`);
+    }
+}
