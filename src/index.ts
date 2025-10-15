4a3600d5fe046606467973e3b4aac0d5ed013210
Pepe <j.reyes@solucionescomsys.com>
2025-10-15 17:35:48 -0600

    [FEAT] protected routes by jwt auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import lastKnownPositionRouter from './api/v1/lastKnownPosition/routes/lastKnownPosition.routes';
+import { verifyJwt } from './middleware/verifyJwt';
 
 dotenv.config();
 
@@ -8,9 +10,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express api running!');
-})
+app.use(verifyJwt);
+
+app.use('/v1', lastKnownPositionRouter);
 
 app.listen(PORT, () => {
     console.log(`Server up on http://localhost:${PORT}`);
